8059b7502d8d0cb7c96fff9cb01d80a634f8f5a6
zSinx6 <lucassin_x@hotmail.com>
2016-12-06 14:30:55 -0200

    animations, wheels and prefabs

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wheel : MonoBehaviour {
+
+	bool up;
+	public bool waitmore;
+
+	// Use this for initialization
+	void Start () {
+		up = true;
+		StartCoroutine (upDown ());
+	}
+
+	IEnumerator upDown(){
+		if (waitmore) {
+			yield return new WaitForSeconds (0.5f);
+		}
+		while (true) {
+			yield return new WaitForSeconds (1f);
+			if (up) {
+				iTween.MoveAdd (gameObject, Vector3.up*2, 1f);
+				up = false;
+			} else {
+				iTween.MoveAdd (gameObject, Vector3.down*2, 1f);
+				up = true;
+			}
+		}	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
