09c4396579fb7a369b03b6e3c2341d8785df6a4e
zSinx6 <lucassin_x@hotmail.com>
2016-12-07 11:41:07 -0200

    fix credits, now go to start screen

---
+++ Assets/Scripts/credits.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class credits : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			Destroy (GameObject.FindGameObjectWithTag ("Persistent"));
+			Application.LoadLevel (0);
+		}
+	}
+}
